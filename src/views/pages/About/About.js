33be0aafbcee1c5117602b0d3beaab8b3f768212
erik-olofsson2 <erik.olofsson2@schibsted.se>
2017-09-14 15:24:45 +0200

    Fix modal overflow scroll

--- src/views/pages/About/About.js
+++ src/views/pages/About/About.js
@@ -25,6 +25,24 @@ class About extends Component {
           onRequestClose={this.toggleModal}
         >
           <button onClick={this.toggleModal}>Close modal</button>
+          <h1>Lorem ipsum</h1>
+          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+          totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae.</p>
+          <p>Dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
+          sed quia consequuntur.</p>
+          <p>Magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
+          qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</p>
+          <p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam,
+          nisi ut aliquid ex ea commodi consequatur</p>
+          <h2>Sit amet</h2>
+          <p>Magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
+          qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</p>
+          <p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam,
+          nisi ut aliquid ex ea commodi consequatur</p>
+          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+          totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae.</p>
+          <p>Dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
+          sed quia consequuntur.</p>
         </ModalBase>
         <button onClick={this.toggleModal}>Open modal</button>
       </main>
